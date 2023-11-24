1bd4adff2d804a6f5cbd2de77387d4e578d13b94
Sniij <120099321+Sniij@users.noreply.github.com>
2023-11-24 23:10:50 +0900

    [Refactoring] Commu, User 진행 중

--- src/main/java/com/codestates/sebmainproject009/user/controller/UserController.java
+++ src/main/java/com/codestates/sebmainproject009/user/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.codestates.sebmainproject009.user.controller;
 
 
-import com.codestates.sebmainproject009.comment.service.CommentService;
-import com.codestates.sebmainproject009.commu.entity.Commu;
-import com.codestates.sebmainproject009.commu.service.CommuService;
 import com.codestates.sebmainproject009.response.SingleResponseDto;
 import com.codestates.sebmainproject009.user.dto.UserPatchDto;
 import com.codestates.sebmainproject009.user.dto.UserPostDto;
@@ -19,7 +16,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import java.net.URI;
-import java.util.List;
 
 
 @RestController
@@ -30,15 +26,9 @@ public class UserController {
 
     private final UserMapper mapper;
 
-    private final CommuService commuService;
-
-    private final CommentService commentService;
-
-    public UserController(UserService userService, UserMapper mapper, CommuService commuService, CommentService commentService) {
+    public UserController(UserService userService, UserMapper mapper) {
         this.userService = userService;
         this.mapper = mapper;
-        this.commuService = commuService;
-        this.commentService = commentService;
     }
 
     @PostMapping("/signup")
