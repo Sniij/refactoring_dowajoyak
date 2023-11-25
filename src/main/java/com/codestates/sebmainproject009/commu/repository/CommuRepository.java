7b89e8844080f8022c245b55766da4c9e6009fa4
Sniij <120099321+Sniij@users.noreply.github.com>
2023-11-25 21:29:48 +0900

    [Refactoring] Commu, User 진행 중

--- src/main/java/com/codestates/sebmainproject009/commu/repository/CommuRepository.java
+++ src/main/java/com/codestates/sebmainproject009/commu/repository/CommuRepository.java
@@ -1,14 +1,12 @@
 package com.codestates.sebmainproject009.commu.repository;
 
-import com.codestates.sebmainproject009.comment.entity.Comment;
 import com.codestates.sebmainproject009.commu.entity.Commu;
-import com.codestates.sebmainproject009.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public interface CommuRepository  extends JpaRepository<Commu, Long> {
 
